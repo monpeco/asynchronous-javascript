3e6fb0833e812638573a7a1d260f9a64d7a2c930
monpeco <monpeco@gmail.com>
2017-07-13 03:39:09 +0000

    Create synchronous exmaple

---
+++ module1/examples/synchronous-processing.js
@@ -0,0 +1,31 @@
+function slowTask(){
+    /*takes 2 seconds to process*/
+    var now = new Date().getTime();
+    while(new Date().getTime() < now + 2000){ /* processing */ } 
+
+
+    console.log("slow task finished");
+};
+
+function fastTask(){
+    console.log("fast task finished")
+};
+
+function init(){
+fastTask();
+slowTask()
+fastTask();
+fastTask();
+fastTask();    
+};
+
+
+/*  Console Output:
+    > "fast task finished"
+    ....2 seconds later
+    > "slow task finished"
+    > "fast task finished"
+    > "fast task finished"
+    > "fast task finished"
+
+*/
