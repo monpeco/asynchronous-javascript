a0153bc715abd0b1863cada416f9829b1b4e1dc6
monpeco <monpeco@gmail.com>
2017-07-25 01:31:58 +0000

    Add handling keypress events (with onkeypress)

--- module1/examples/handling-keypress-events.js
+++ module1/examples/handling-keypress-events.js
@@ -1,14 +1,24 @@
 var value10 = 0;
-
 function init(){
     
     console.log("handling-keypress-events.js");
 
-    document.addEventListener('keypress', handleKeyPress);
+    // method 1: with addEventListener
+    // document.addEventListener('keypress', handleKeyPress);
 
-    function handleKeyPress(event){
-        var keypressed = event.key;
-        document.getElementById("text").innerHTML = "Key Pressed: " + keypressed;
-    }
+    // function handleKeyPress(event){
+    //     var keypressed = event.key;
+    //     document.getElementById("text").innerHTML = "Key Pressed: " + keypressed;
+    // }
+
+    // method 2: with onkeypress
+    document.onkeypress = handleKeyPress;
+
 }
 
+
+// method 2: with onkeypress
+function handleKeyPress(event){
+         var keypressed = event.key;
+         document.getElementById("text").innerHTML = "Key Pressed: " + keypressed;
+}
