7eb4d7e3ef4811b6d8c6f544c0fccbf0fd018b9c
monpeco <monpeco@gmail.com>
2017-08-22 01:13:56 +0000

    Create and use a promise (then and catch)

--- module2/code-demo/promises01.js
+++ module2/code-demo/promises01.js
@@ -3,3 +3,26 @@ window.onload = init;
 function init(){
     console.log("promises01.js");
 }
+
+
+var promise = new Promise(function(resolve, reject){
+
+    setTimeout(function(){
+        var val = Math.random();
+        if(val > 0.5){
+            resolve(val);
+        }else{
+            reject(val);
+        }
+    },1000);
+
+    
+});
+
+
+promise.then(function(val){
+    console.log("success : " + val);
+}).catch(function(err){
+    console.log("failure : " + err);
+
+});
