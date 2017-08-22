91e76f09c477f490c53fcc6240afe03ba30821d6
monpeco <monpeco@gmail.com>
2017-08-22 01:56:36 +0000

    Add how to promise's result is changed with the return statement

--- module2/examples/promise-return01.js
+++ module2/examples/promise-return01.js
@@ -5,3 +5,13 @@ function init(){
 }
 
 
+var promise = Promise.resolve("hello");
+
+var promise2 = promise.then(function(result){
+    console.log(result);       //logs "hello"
+    return result + " world";  //adds " world" to the result and sets this as the new fulfillment value of promise2
+});
+
+promise2.then(function(result){
+    console.log(result);       //logs "hello world"
+});
