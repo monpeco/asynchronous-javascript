a8d3bdb4e8b3012a6054e3cdc812784f4eb3a428
monpeco <monpeco@gmail.com>
2017-08-25 17:01:22 +0000

    Move createRandomProduct and createRandomCatalog definitions

--- module2/lab/library.js
+++ module2/lab/library.js
@@ -13,7 +13,22 @@
         }
 
         //function definitions go here
-
+        function createRandomProduct(){
+            var typeArray = ['Electronics','Book','Clothing','Food'];
+            var price = (Math.random()*500).toFixed(2) 
+            var type = typeArray[Math.floor(Math.random()*4)];
+        
+            return {price:price, type:type};                
+        }
+        
+        function createRandomCatalog(num){
+            var catalog = [];
+            for (var i = 0; i < num; i++){
+                var obj = createRandomProduct();
+                catalog.push({id:i,price:obj.price,type:obj.type});
+            }
+            return catalog;
+        }
 
     }
 
@@ -24,19 +39,5 @@
 
 })(window); 
 
-function createRandomProduct(){
-    var typeArray = ['Electronics','Book','Clothing','Food'];
-    var price = (Math.random()*500).toFixed(2) 
-    var type = typeArray[Math.floor(Math.random()*4)];
 
-    return {price:price, type:type};                
-}
 
-function createRandomCatalog(num){
-    var catalog = [];
-    for (var i = 0; i < num; i++){
-        var obj = createRandomProduct();
-        catalog.push({id:i,price:obj.price,type:obj.type});
-    }
-    return catalog;
-}
