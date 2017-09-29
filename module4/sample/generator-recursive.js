68eb09a8c28cec6381d00adaa65cff5d9f5ff5f4
monpeco <monpeco@gmail.com>
2017-09-29 02:20:25 +0000

    Use run function to iterate over gen* (a function generator)

--- module4/sample/generator-recursive.js
+++ module4/sample/generator-recursive.js
@@ -16,3 +16,36 @@ function run(genFunc){
     }
 }
 
+
+function *gen(){
+
+        var post1Stream = yield fetch("https://jsonplaceholder.typicode.com/posts/1");
+        var post1 = yield post1Stream.json();
+        console.log(post1.title); 
+        //post1.title = "sunt aut facere repellat provident occaecati excepturi optio reprehenderit"
+        var post2Stream = yield fetch("https://jsonplaceholder.typicode.com/posts/2");
+        var post2 = yield post2Stream.json();
+        console.log(post2.title);
+        //post2.title = "qui est esse"
+
+        var number = yield 12345;
+        console.log(number)
+        //number = 12345
+
+        var string = yield "abc";
+        console.log(string)
+        //string = "abc"
+
+        var obj = yield {id:123,name:"xyz"};
+        console.log(obj)
+        //obj = Object {id:123,name:"xyz"}
+
+        var a = yield 54434337746;
+        console.log(a);
+        return "done";
+
+}
+
+
+run(gen).then(x => console.log(x))          //logs done
+    .catch(x => console.log(x.message));
